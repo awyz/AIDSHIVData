4eb421f2db95df73eae0ed5a97ce91af8c48d50b
blaine-wataru <blainewataru@gmail.com>
2018-11-21 12:25:33 -1000

    Add Placeholder text and descriptions

--- Sociodemographics.js
+++ Sociodemographics.js
@@ -1,6 +1,6 @@
-var ageChart = echarts.init(document.getElementById('age'));
+var ageChartSurvey = echarts.init(document.getElementById('ageSurvey'));
 
-var options = {
+var ageOptionsSurvey = {
     title : {
         text: 'Survey',
         x: 'center'
@@ -29,4 +29,39 @@ var options = {
     ]
 };
 
-ageChart.setOption(options);
+ageChartSurvey.setOption(ageOptionsSurvey);
+
+
+var ageChartProfile = echarts.init(document.getElementById('ageProfile'));
+
+var ageOptionsProfile = {
+    title : {
+        text: 'Epi Profile',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Ages in Survey',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+
+ageChartProfile.setOption(ageOptionsProfile);
+
