8d03ab155facb610fdd97de2f0fed236e03ac9d9
blaine-wataru <blainewataru@gmail.com>
2018-11-21 13:29:06 -1000

    Implement all Placeholder graphs and text

--- Sociodemographics.js
+++ Sociodemographics.js
@@ -1,5 +1,5 @@
+// Age
 var ageChartSurvey = echarts.init(document.getElementById('ageSurvey'));
-
 var ageOptionsSurvey = {
     title : {
         text: 'Survey',
@@ -28,12 +28,9 @@ var ageOptionsSurvey = {
         }
     ]
 };
-
 ageChartSurvey.setOption(ageOptionsSurvey);
 
-
 var ageChartProfile = echarts.init(document.getElementById('ageProfile'));
-
 var ageOptionsProfile = {
     title : {
         text: 'Epi Profile',
@@ -50,7 +47,40 @@ var ageOptionsProfile = {
     },
     series: [
         {
-            name: 'Distribution of Ages in Survey',
+            name: 'Distribution of Ages in Epi Profile',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+ageChartProfile.setOption(ageOptionsProfile);
+
+
+// Disabilities
+var disabilityChartSurvey = echarts.init(document.getElementById('disabilitySurvey'));
+var disabilityOptionsSurvey = {
+    title : {
+        text: 'Survey',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Disabilities in Survey',
             type: 'pie',
             radius: '55%',
             center: ['50%', '60%'],
@@ -63,5 +93,233 @@ var ageOptionsProfile = {
     ]
 };
 
-ageChartProfile.setOption(ageOptionsProfile);
+disabilityChartSurvey.setOption(disabilityOptionsSurvey);
+
+var disabilityChartProfile = echarts.init(document.getElementById('disabilityProfile'));
+var disabilityOptionsProfile = {
+    title : {
+        text: 'Epi Profile',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Disabilities in Epi Profile',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+disabilityChartProfile.setOption(disabilityOptionsProfile);
+
+
+// Drug Use
+var drugUseChartSurvey = echarts.init(document.getElementById('drugUseSurvey'));
+var drugUseOptionsSurvey = {
+    title : {
+        text: 'Survey',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Drug Use in Survey',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+
+drugUseChartSurvey.setOption(drugUseOptionsSurvey);
+
+var drugUseChartProfile = echarts.init(document.getElementById('drugUseProfile'));
+var drugUseOptionsProfile = {
+    title : {
+        text: 'Epi Profile',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Drug Use in Epi Profile',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+drugUseChartProfile.setOption(drugUseOptionsProfile);
+
+
+// Income Type
+var incomeTypeChartSurvey = echarts.init(document.getElementById('incomeTypeSurvey'));
+var incomeTypeOptionsSurvey = {
+    title : {
+        text: 'Survey',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Income Type in Survey',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+
+incomeTypeChartSurvey.setOption(incomeTypeOptionsSurvey);
+
+var incomeTypeChartProfile = echarts.init(document.getElementById('incomeTypeProfile'));
+var incomeTypeOptionsProfile = {
+    title : {
+        text: 'Epi Profile',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Income Type in Epi Profile',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+incomeTypeChartProfile.setOption(incomeTypeOptionsProfile);
+
+
+// Sexual Orientation
+var sexualOrientationChartSurvey = echarts.init(document.getElementById('sexualOrientationSurvey'));
+var sexualOrientationOptionsSurvey = {
+    title : {
+        text: 'Survey',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Sexual Orientation in Survey',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+
+sexualOrientationChartSurvey.setOption(sexualOrientationOptionsSurvey);
+
+var sexualOrientationChartProfile = echarts.init(document.getElementById('sexualOrientationProfile'));
+var sexualOrientationOptionsProfile = {
+    title : {
+        text: 'Epi Profile',
+        x: 'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['Placeholder1', 'Placeholder2', 'Placeholder3']
+    },
+    series: [
+        {
+            name: 'Distribution of Sexual Orientation in Epi Profile',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                {value:69, name: 'Placeholder1'},
+                {value:69, name: 'Placeholder2'},
+                {value:69, name: 'Placeholder3'}
+            ],
+        }
+    ]
+};
+sexualOrientationChartProfile.setOption(sexualOrientationOptionsProfile);
+
+
 
