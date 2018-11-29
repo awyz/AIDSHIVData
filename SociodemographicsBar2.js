c97d303204095afd0da02364f8c273278279babb
blaine-wataru <blainewataru@gmail.com>
2018-11-28 16:40:57 -1000

    Attempts to fix things

--- SociodemographicsBar2.js
+++ SociodemographicsBar2.js
@@ -26,6 +26,34 @@ var myChart = new Chart(ageChart, {
     // options: options
 });
 
+var ageChart = document.getElementById('caseManagement');
+var myChart = new Chart(ageChart, {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: 'HIV Needs Assessment',
+                data: [4, 12, 6, 5, 1, 3],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(153, 102, 255)',
+                    'rgb(255, 159, 64)'
+                ]},
+            {
+                label: 'Epi Profile',
+                data: [1, 2, 3, 4, 5, 6],
+            }
+
+        ]
+    },
+    options: createOptions('Percentage of People with HIV', 'Age', 398)
+    // options: options
+});
+
 function createOptions(xAxisLabel, yAxisLabel, nValue) {
 
     var options = {
